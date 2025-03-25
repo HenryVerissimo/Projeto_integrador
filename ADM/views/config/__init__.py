904180691390fdaf83838f7ab7ffff7b33db2b10
HenryVerissimo <henriquedevsp@gmail.com>
2025-03-25 12:57:28 -0300

    Apagando arquivos de ADM para recomeçar e refatorar

--- ADM/views/config/__init__.py
+++
@@ -1,1 +0,0 @@
-from .settings_page import AddConfigPage, DefaultConfig
--- ADM/views/config/interface_view.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class InterfaceView(ABC):
-
-    @abstractmethod
-    def build(self):
-        pass
